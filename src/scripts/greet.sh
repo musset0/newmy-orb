bd83a4e3063d969cb33bf8938560a6015d75e331
musset0 <138792596+musset0@users.noreply.github.com>
2023-11-21 15:30:32 -0500

    Update greet.sh

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -5,4 +5,5 @@ TO=$(circleci env subst "${PARAM_TO}")
 # If for any reason the TO variable is not set, default to "World"
 echo "Hello ${TO:-World}!"
 echo "v1.0.1"
-cat /etc/opt/circleci/launch-agent-config.yaml
+# cat /etc/opt/circleci/launch-agent-config.yaml
+echo ${Project_First_Var}
